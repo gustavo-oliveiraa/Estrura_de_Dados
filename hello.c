0a2119b9ac745058335433b555cad8c11091ee82
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-03-24 10:55:00 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main(){
+
+    // var
+    // I/O - scanf / printf
+    // alocação de espaços em memória...
+    // nome, tipo, valor *tamanho
+    // nome (indentificador): sem espaço, char especiais, 
+    // tipo primitivos - int, char, float(6), double, (longint)
+    // valores... 5 '5' "5" 5.0
+
+    // operadores
+    // aritméticos: + - * / %
+    // == = i++(salto) ++i i-=3
+
+    // Estruturas
+
+    // Condicionais
+    // if, if/else, switch...
+
+    // Repetição
+    // for, while, do..while
+
+    // Estrutura de dados compostas homogênea
+    // -> vetores, strings, matrizes
+        
+    // Estrutura de dados compostas heterogêneas
+    // -> struct (struct de struct?, vetor de struct)
+    // typedef (definição de tipo)
+
+    // ponteiros
+
+    // manipulação de arquivos?
+
+    // modularização
+    // funções e procedimentos
+
+    printf("Hello, World!");
+
+    return 0;
+}
